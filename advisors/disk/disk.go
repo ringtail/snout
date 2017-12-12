8595e3de584b4e5b195dff5c59c4acd496c6da5d
ringtail <zhongwei.lzw@alibaba-inc.com>
2017-12-12 16:06:42 +0800

    add disk advisor

---
+++ advisors/disk/disk.go
@@ -0,0 +1,17 @@
+package disk
+
+//TODO 
+type DiskAdvisor struct {
+}
+
+func (da *DiskAdvisor) Name() string {
+	return ""
+}
+
+func (da *DiskAdvisor) Description() string {
+	return ""
+}
+
+func (da *DiskAdvisor) Advise() string {
+	return ""
+}
